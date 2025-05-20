a1449cb804a62a6d42af3ee8a25e209427a66ff0
eeman122 <24280022@lums.edu.pk>
2025-05-20 23:42:05 +0500

    Update llm_project.py

--- llm_project.py
+++ llm_project.py
@@ -521,7 +521,7 @@ with gr.Blocks() as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=port)
 
 
 
